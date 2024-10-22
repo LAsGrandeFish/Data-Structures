d3124758ed0cb0304ff24448790367e9d5f64725
Ivan Zapote <ivanzapote1331@gmail.com>
2024-10-22 18:54:41 -0400

    added function declarations to singly_linked_list.h; functions added are the same as the ones seen in the python implementation of a singly linked list

--- C_Practice/Singly_Linked_List/singly_linked_list.h
+++ C_Practice/Singly_Linked_List/singly_linked_list.h
@@ -12,6 +12,19 @@ typedef struct SinglyLinkedList
 } SinglyLinkedList;
 
 SinglyLinkedList* create_singly_linked_list();
+
 int is_empty(SinglyLinkedList* sll);
+void add_to_back(SinglyLinkedList* sll, int data);
+void add_to_front(SinglyLinkedList* sll, int data);
+void insert_after_node(SinglyLinkedList* sll, int prev_node_data, int data);
+void delete_node(SinglyLinkedList* sll, int data);
+void delete_node_at_position(SinglyLinkedList* sll, int position);
+int search(SinglyLinkedList* sll, int key);
+int* display(SinglyLinkedList* sll);
+void reverse(SinglyLinkedList* sll);
+
+Node* get_head(SinglyLinkedList* sll);
+Node* get_tail(SinglyLinkedList* sll);
+int get_size(SinglyLinkedList* sll);
 
 #endif // SINGLY_LINKED_LIST_H
