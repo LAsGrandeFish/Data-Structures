532f086c85d97f4cfcafaacf71b191219172179c
Ivan Zapote <ivanzapote1331@gmail.com>
2024-10-22 19:19:22 -0400

    implemented function add_to_back(); untested

--- C_Practice/Singly_Linked_List/singly_linked_list.c
+++ C_Practice/Singly_Linked_List/singly_linked_list.c
@@ -3,11 +3,9 @@
 #include "singly_linked_list.h"
 #include "node.h"
 
-SinglyLinkedList* create_singly_linked_list()
-{
+SinglyLinkedList* create_singly_linked_list() {
     SinglyLinkedList* sll = (SinglyLinkedList*)malloc(sizeof(SinglyLinkedList));
-    if (sll == NULL)
-    {
+    if (sll == NULL) {
         printf("Memory allocation for singly linked list fail");
         exit(1);
     }
@@ -17,7 +15,20 @@ SinglyLinkedList* create_singly_linked_list()
     return sll;
 }
 
-int is_empty(SinglyLinkedList* sll)
-{
+int is_empty(SinglyLinkedList* sll) {
     return sll->size == 0;
+}
+
+void add_to_back(SinglyLinkedList* sll, int data) {
+    if (sll == NULL || data == NULL) {
+        exit(1);
+    }
+    Node* new_node = create_node(data);
+    if (sll->size == 0) {
+        sll->head = new_node;
+    } else {
+        set_next(sll->tail, new_node);
+    }
+    sll->tail = new_node;
+    sll->size++; 
 }
